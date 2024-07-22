1ad84d1ed87907aa865cbaf23e141bb0f01f9a81
amalkin <102127060+anmalkin@users.noreply.github.com>
2024-07-22 10:04:12 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
-use std::net::TcpListener;
+use std::{io::Write, net::{TcpListener, TcpStream}};
+
+const STATUS_200: &[u8] = b"HTTP/1.1 200 OK\r\n\r\n";
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let mut handles = Vec::new();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                let handle = std::thread::spawn(move || handle_connection(stream));
+                handles.push(handle);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    match stream.write_all(STATUS_200) {
+        Ok(_) => {},
+        Err(e) => println!("Error: {}", e),
     }
 }
