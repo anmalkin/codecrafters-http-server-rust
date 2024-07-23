d805df5c56dbbca5bd5e9ccacc173d090cac3471
amalkin <102127060+anmalkin@users.noreply.github.com>
2024-07-22 19:56:00 -0700

    Add support for HTTP response parsing and request handling

--- src/http.rs
+++ src/http.rs
@@ -1,6 +1,9 @@
-use std::path::PathBuf;
+#![allow(dead_code)]
 
 use bytes::{BufMut, Bytes, BytesMut};
+use std::{path::PathBuf, str::FromStr};
+
+use crate::errors::HttpError;
 
 #[derive(Debug)]
 pub enum HttpProtocol {
@@ -11,31 +14,31 @@ pub enum HttpProtocol {
 impl HttpProtocol {
     pub fn as_bytes(&self) -> Bytes {
         match self {
-            HttpProtocol::Http1_1 => Bytes::from("HTTP/1.1 "),
-            HttpProtocol::Http1_0 => Bytes::from("HTTP/1.0 "),
+            HttpProtocol::Http1_1 => Bytes::from(&b"HTTP/1.1 "[..]),
+            HttpProtocol::Http1_0 => Bytes::from(&b"HTTP/1.0 "[..]),
         }
     }
 }
 
 pub enum HttpStatusCode {
     _200_,
-    _400_,
+    _404_,
 }
 
 impl HttpStatusCode {
     pub fn as_bytes(&self) -> Bytes {
         match self {
-            HttpStatusCode::_200_ => Bytes::from("200 OK"),
-            HttpStatusCode::_400_ => Bytes::from("404 Not Found"),
+            HttpStatusCode::_200_ => Bytes::from(&b"200 OK"[..]),
+            HttpStatusCode::_404_ => Bytes::from(&b"404 Not Found"[..]),
         }
     }
 }
 
 #[derive(Debug)]
 pub enum HttpMethod {
-    GET,
-    PUT,
-    POST,
+    Get,
+    Put,
+    Post,
 }
 
 #[derive(Debug)]
@@ -48,12 +51,12 @@ impl HttpResponseHeader {
     pub fn as_bytes(&self) -> Bytes {
         match self {
             HttpResponseHeader::ContentType(str) => {
-                let mut header = BytesMut::from("Content-Type: ");
+                let mut header = BytesMut::from(&b"Content-Type: "[..]);
                 header.put(&str[..]);
                 header.freeze()
             }
             HttpResponseHeader::ContentLength(n) => {
-                let mut header = BytesMut::from("Content-Length: ");
+                let mut header = BytesMut::from(&b"Content-Length: "[..]);
                 header.put(&n[..]);
                 header.freeze()
             }
@@ -61,25 +64,19 @@ impl HttpResponseHeader {
     }
 }
 
-#[derive(Debug)]
-pub enum HttpRequestHeader {
-    Host(Bytes),
-    UserAgent(Bytes),
-}
-
-struct HttpResponse {
+pub struct HttpResponse<'a> {
     protocol: HttpProtocol,
     status: HttpStatusCode,
     headers: Vec<HttpResponseHeader>,
-    body: BytesMut,
+    body: &'a str,
 }
 
-impl HttpResponse {
+impl<'a> HttpResponse<'a> {
     pub fn new(
         protocol: HttpProtocol,
         status: HttpStatusCode,
         headers: Vec<HttpResponseHeader>,
-        body: BytesMut,
+        body: &'a str,
     ) -> Self {
         Self {
             protocol,
@@ -87,6 +84,22 @@ impl HttpResponse {
             headers,
             body,
         }
+    }
+
+    pub fn status(&mut self, status: HttpStatusCode) {
+        self.status = status;
+    }
+
+    pub fn header(&mut self, header: HttpResponseHeader) {
+        self.headers.push(header);
+    }
+
+    pub fn headers(&mut self, headers: &mut Vec<HttpResponseHeader>) {
+        self.headers.append(headers);
+    }
+
+    pub fn body(&mut self, body: &'a str) {
+        self.body = body;
     }
 
     pub fn as_bytes(&self) -> Bytes {
@@ -99,18 +112,17 @@ impl HttpResponse {
             response.put("\r\n".as_bytes());
         }
         response.put("\r\n".as_bytes());
-        response.put(self.body.clone());
+        response.put(self.body.as_bytes());
         response.freeze()
     }
 }
 
-impl Default for HttpResponse {
+impl<'a> Default for HttpResponse<'a> {
     fn default() -> Self {
         let protocol = HttpProtocol::Http1_1;
         let status = HttpStatusCode::_200_;
-        let content_len = Bytes::from("0");
-        let headers = vec![HttpResponseHeader::ContentLength(content_len)];
-        let body = BytesMut::new();
+        let headers = Vec::new();
+        let body = "";
         Self {
             protocol,
             status,
@@ -120,9 +132,72 @@ impl Default for HttpResponse {
     }
 }
 
-pub struct HttpRequest {
-    method: HttpMethod,
-    path: PathBuf,
-    protocol: HttpProtocol,
-    headers: Vec<HttpRequestHeader>,
+pub struct HttpRequest<'a> {
+    pub method: HttpMethod,
+    pub path: PathBuf,
+    pub protocol: HttpProtocol,
+    pub headers: Vec<&'a str>,
+}
+
+impl<'a> HttpRequest<'a> {
+    pub fn parse(bytes: &'a BytesMut) -> Result<Self, HttpError> {
+        let request = std::str::from_utf8(bytes.as_ref())?;
+        let mut lines = request.lines();
+        let mut start_line = lines
+            .next()
+            .ok_or(HttpError::InvalidRequestFormat)?
+            .split_whitespace();
+
+        let method = start_line
+            .next()
+            .ok_or(HttpError::InvalidRequestFormat)?
+            .parse::<HttpMethod>()?;
+
+        let path = std::path::Path::new(start_line.next().ok_or(HttpError::InvalidRequestFormat)?)
+            .to_owned();
+
+        let protocol = start_line
+            .next()
+            .ok_or(HttpError::InvalidRequestFormat)?
+            .parse::<HttpProtocol>()?;
+
+        let mut headers = Vec::new();
+        let mut curr_line = lines.next().ok_or(HttpError::InvalidRequestFormat)?;
+        while !curr_line.is_empty() {
+            headers.push(curr_line);
+            curr_line = lines.next().ok_or(HttpError::InvalidRequestFormat)?;
+        }
+        Ok(Self {
+            method,
+            path,
+            protocol,
+            headers,
+        })
+    }
+}
+
+// Ergonomic trait implementations
+impl FromStr for HttpProtocol {
+    type Err = HttpError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "HTTP/1.1" => Ok(HttpProtocol::Http1_1),
+            "HTTP/1.0" => Ok(HttpProtocol::Http1_0),
+            _ => Err(HttpError::ParseProtocolError),
+        }
+    }
+}
+
+impl FromStr for HttpMethod {
+    type Err = HttpError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_uppercase().as_str() {
+            "GET" => Ok(HttpMethod::Get),
+            "PUT" => Ok(HttpMethod::Put),
+            "POST" => Ok(HttpMethod::Post),
+            _ => Err(HttpError::ParseMethodError),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use std::{
     io::{Read, Write},
     net::{TcpListener, TcpStream},
-    str,
 };
 
+mod errors;
 mod http;
 
-use crate::http::*;
+use bytes::{Bytes, BytesMut};
 
-const STATUS_200: &[u8] = b"HTTP/1.1 200 OK\r\n\r\n";
-const STATUS_404: &[u8] = b"HTTP/1.1 404 Not Found\r\n\r\n";
+use crate::http::*;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -34,7 +33,7 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buf: Vec<u8> = vec![0; 512];
+    let mut buf = BytesMut::from(&[0; 512][..]);
     loop {
         let n = stream.read(&mut buf).unwrap();
 
@@ -42,25 +41,44 @@ fn handle_connection(mut stream: TcpStream) {
             break;
         }
 
-        // TODO: Better error handling
-        let mut str = str::from_utf8(&buf[..]).unwrap().lines();
-        let mut start_line = str.next().unwrap().split_whitespace();
-        let method = start_line.next().unwrap();
-        let target = start_line.next().unwrap();
-        match target {
-            "/" => match stream.write(STATUS_200) {
-                Ok(_) => {}
-                Err(e) => eprintln!("Error: {}", e),
-            },
-            _ => match stream.write(STATUS_404) {
-                Ok(_) => {}
-                Err(e) => eprintln!("Error: {}", e),
-            },
-        }
-    }
+        let request = match HttpRequest::parse(&buf) {
+            Ok(request) => request,
+            Err(e) => {
+                eprintln!("{}", e);
+                continue;
+            }
+        };
 
-    match stream.write_all(STATUS_200) {
-        Ok(_) => {}
-        Err(e) => eprintln!("Error: {}", e),
+        let mut response = HttpResponse::default();
+
+        match request.method {
+            HttpMethod::Get => {
+                if !request.path.has_root() {
+                    response.status(HttpStatusCode::_404_);
+                }
+                if let Some(path) = request.path.parent() {
+                    match path.to_str().unwrap() {
+                        "/echo" => {
+                            let msg = request.path.file_stem().unwrap().to_str().unwrap();
+                            response.body(msg);
+                            response
+                                .header(HttpResponseHeader::ContentType(Bytes::from("text/plain")));
+                            response.header(HttpResponseHeader::ContentLength(Bytes::from(
+                                msg.len().to_string(),
+                            )));
+                        }
+                        _ => response.status(HttpStatusCode::_404_),
+                    }
+                }
+            }
+            _ => response.status(HttpStatusCode::_404_),
+        }
+        match stream.write_all(response.as_bytes().as_ref()) {
+            Ok(_) => continue,
+            Err(e) => {
+                eprintln!("{}", e);
+                break;
+            }
+        }
     }
 }
